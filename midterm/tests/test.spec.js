f6a2c0b689e7ff4137dd2eb90af47b706fdfdfe4
NUTTAMAS UDOMSANTITHAM <6733064521@student.chula.ac.th>
2026-03-05 22:57:56 +0700

    update remove subject

--- midterm/tests/test.spec.js
+++ midterm/tests/test.spec.js
@@ -46,6 +46,54 @@ test("submit success with full data", async ({ page }) => {
   await expect(dialog).toBeHidden();
 });
 
+test("remove subject", async ({ page }) => {
+
+  await page.goto("https://demoqa.com/automation-practice-form");
+  
+  await expect(page.locator("#firstName")).toBeVisible();
+
+  // first name //
+  await page.locator("#firstName").fill("Nuttamas");
+  // last name //
+  await page.locator("#lastName").fill("Udomsantitham");
+  // email //
+  await page.locator("#userEmail").fill("nt@test.com");
+  // gender //
+  await page.locator("#gender-radio-2").click();
+  // number //
+  await page.locator("#userNumber").fill("0899999999");
+  // date of birth
+  await page.locator("#dateOfBirthInput").click();
+  await page.locator(".react-datepicker__month-select").selectOption("11");
+  await page.locator(".react-datepicker__year-select").selectOption("2005");
+  await page.locator(".react-datepicker__day--009:not(.react-datepicker__day--outside-month)").click();
+  // subjects
+  await page.locator("#subjectsInput").fill("Maths");
+  await page.getByText('Maths', { exact: true }).click();
+  await page.locator("#subjectsInput").fill("Eng");
+  await page.getByText('English', { exact: true }).click();
+  // remove subject
+  await page.getByLabel("Remove Maths").click();
+  // hobbies
+  await page.getByRole("checkbox", { name: "Music" }).check();
+  // current address
+  await page.locator("#currentAddress").fill("Test Current Address");
+  // state and city
+  await page.locator("#react-select-3-input").click();
+  await page.getByText('NCR', { exact: true }).click();
+  await page.locator("#react-select-4-input").click();
+  await page.getByText('Delhi', { exact: true }).click();
+  // submit
+  await page.getByRole("button", { name : "Submit" }).click()
+  await expect(page.getByText("Thanks for submitting the form", { exact: true })).toBeVisible();
+  // assert modal appeared
+  const dialog = page.getByRole("dialog");
+  await expect(dialog).toBeVisible();
+  // close modal 
+  await page.mouse.click(0, 0);
+  await expect(dialog).toBeHidden();
+});
+
 test("no first name", async ({ page }) => {
 
   await page.goto("https://demoqa.com/automation-practice-form");
